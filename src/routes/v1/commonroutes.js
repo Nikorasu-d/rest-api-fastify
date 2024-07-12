b2c5de25068891a10b5d0c05d4f32f296538f6da
nikorasu-d <nicolas.lefgut@gmail.com>
2024-07-12 17:04:28 -0400

    Prettying

--- src/routes/v1/commonroutes.js
+++ src/routes/v1/commonroutes.js
@@ -1,15 +1,10 @@
 const { getAllTodos, getTodo, postTodo, putTodo, deleteTodo } = require("../../controller/todoController")
 
 const routes = async (app, options) =>{
-
     app.get("/todos/all", getAllTodos)
-
     app.get("/todos/:id", getTodo)
-
     app.post("/todos", postTodo)
-
     app.put("/todos",putTodo)
-
     app.delete("/todos/:id", deleteTodo)
 }
 
